a85c5ab9489dcd3aa3e24472994e499fa10f295a
Ayuba Sherif Deen <ayubasherifdeen@gmail.com>
2026-08-21 13:02:35 +0000

    padding fixes

--- start.sh
+++ start.sh
@@ -8,11 +8,6 @@ pip install -q -r ../requirements.txt
 
 # Run Django migrations
 python manage.py migrate --noinput
-#python manage.py seed_data
-DJANGO_SUPERUSER_USERNAME=admin \
-DJANGO_SUPERUSER_EMAIL=admin@inventiq.com \
-DJANGO_SUPERUSER_PASSWORD=3_SEAurchins \
-python manage.py createsuperuser --noinput   
 
 # Collect static files
 python manage.py collectstatic --noinput
